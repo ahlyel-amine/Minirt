e4a61f587387b0dca41b91e86987bb1818ca8681
AhlyelAmine <aa.ahlyel.amine@gmail.com>
2023-08-04 03:11:15 +0100

    backup

---
+++ a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int a;
+	// a = 4;
+	if ((a = 10 < 7))
+	{
+        printf("a = %d\n", a);
+    }
+        printf("a = %d\n", a);
+	return 0;
+}
