115bd0fbdcfa94baffb7e76a39289a5910d48dd0
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-08-04 02:04:50 +0100

    backup working env

--- libft/obj/ft_lstdelone.d
+++
@@ -1,3 +0,0 @@
-obj/ft_lstdelone.o: src/ft_lstdelone.c src/../include/libft.h
-
-src/../include/libft.h:
--- obj/src/parcer/scene/lighting_parcer.d
+++
@@ -1,4 +0,0 @@
-obj/src/parcer/scene/lighting_parcer.o: \
-  src/parcer/scene/lighting_parcer.c include/structs.h
-
-include/structs.h:
