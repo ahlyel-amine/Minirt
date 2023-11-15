bbc5a143c107eccb6991a4f1dc7ec591e4b55c54
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-11-15 06:25:53 +0100

    implement multi threading

--- include/tools.h
+++ include/tools.h
@@ -6,6 +6,7 @@
 int			skip_spaces(char *str);
 void		get_integer(char **line, int *integer);
 void		addobject_front(t_objects **lst, t_objects *new);
+void    	addobject_back(t_objects **lst, t_objects *new);
 double		ft_atod(char **ascii, bool *success, int marge_max, int marge_min);
 t_objects	*newobject(void *object, unsigned char type);
 
