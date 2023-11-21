ab0dc18080d09055cc5001d3059d7032d4b60dbc
bleuuone <bleuuone@gmail.com>
2023-11-21 14:31:01 +0100

    FIX_CYLINDER

--- src/minirt/intersection.c
+++ src/minirt/intersection.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:03:01 by aelbrahm          #+#    #+#             */
-/*   Updated: 2023/11/20 11:16:47 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/11/21 14:29:55 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ bool	sphere_hit(t_ray *ray, t_objects *obj, t_hit_record *rec)
 	}
 	rec->t = tmp;
 	rec->pHit = at(rec->t, *ray);
-	t_vec norm = scalar_div(vec_sub(rec->pHit, sphere->cord), pow(sphere->diameter/2, 2));
+	t_vec norm = scalar_div(vec_sub(rec->pHit, sphere->cord), pow(sphere->diameter * 0.5, 2));
 	// if (dot_product(norm, norm) == 0.0)
 	// 	return (false);
 	rec->nHit = normalize(&norm);
@@ -80,20 +80,19 @@ bool	plan_hit(t_ray *ray, t_objects *obj, t_hit_record *rec)
 	double	t;
 	t_vec	p;
 	t_plane *plan = obj->object;
-	
+	// normalize(&plan->normalized);
 	denom = dot_product(plan->normalized, ray->direction);
 	if (fabs(denom) < eps)
 		return (false);
-	if (fabs(denom) > eps)
+	if (fabs(denom) >= eps)
 	{
 		p = vec_sub(plan->cord, ray->origin);
 		t = dot_product(p, plan->normalized) / denom;
-		if (t >= eps)
+		if (t > eps)
 		{
 			rec->t = t;
 			rec->pHit = at(rec->t, *ray);
 			rec->nHit = plan->normalized;
-			// printf("normal %f %f %f\n", rec->nHit.v_x, rec->nHit.v_y, rec->nHit.v_z);
 			if (dot_product(rec->nHit, ray->direction) > 0.0)
 				rec->nHit = vec_nega(rec->nHit);
 			rec->h_color = create_vec((double)(plan->clr.r) / 255, (double)(plan->clr.g) / 255, (double)(plan->clr.b) / 255);
@@ -105,73 +104,92 @@ bool	plan_hit(t_ray *ray, t_objects *obj, t_hit_record *rec)
 int isZero(double value, double epsilon) {
     return fabs(value) < epsilon;
 }
-bool	cylinder_hit(t_ray *ray, t_objects *obj, t_hit_record *rec)
+
+bool	solve_quad(t_ray *ray, t_cylender *cylinder, t_hit_record *rec)
 {
-	t_vec	ro;
-	t_cord	p;
-	t_vec u;
+	t_vec orthg;
 	t_vec v;
-	double	discriminant;
-	double	tmp;
-	double	t1;
-	double	t2;
-	t_cylender *cylinder = obj->object;
-	u = cross_product(ray->direction, cylinder->normalized);
-	v = vec_sub(ray->origin, cylinder->cord);
-	v = cross_product(v, cylinder->normalized);
-	p.a = dot_product(u, u);
-	p.b = 2 * dot_product(u, v);
-	p.c = dot_product(v, v) - pow(cylinder->diameter/2, 2);
-	discriminant = p.b * p.b -  4 * p.a * p.c;
-	t1 = (-p.b - sqrt(discriminant)) / (2 * p.a);
-	t2 = (-p.b + sqrt(discriminant)) / (2 * p.a);
-	if (discriminant <= eps || (t1 <= eps && t2 <= eps))
+	t_cord p;
+	t_cord param;
+
+	//pram.a => discriminant, param.b => solution 1, param.c => solution 2
+	orthg = cross_product(ray->direction, cylinder->normalized);
+	v = cross_product(vec_sub(ray->origin, cylinder->cord), cylinder->normalized);
+	p.a = dot_product(orthg, orthg);
+	p.b = 2 * dot_product(orthg, v);
+	p.c = dot_product(v, v) - pow(cylinder->diameter * 0.5, 2);
+	param.a = p.b * p.b -  4 * p.a * p.c;
+	param.b = (-p.b - sqrt(param.a)) / (2 * p.a);
+	param.c = (-p.b + sqrt(param.a)) / (2 * p.a);
+	if (param.a <= eps || (param.b <= eps && param.c <= eps))
 		return (false);
-	if (t1 <= eps || (t2 > eps && (t2 < t1)))
-		t1 = t2;
-	rec->t = t1;
-	rec->pHit = at(rec->t, *ray);
-	v = vec_sub(cylinder->cord, rec->pHit);
-	rec->nHit = cross_product(v, cylinder->normalized);
-	rec->nHit = cross_product(rec->nHit, cylinder->normalized);
-	normalize(&rec->nHit);
-	// if (dot_product(rec->nHit, ray->direction))
-	// 	rec->nHit = vec_nega(rec->nHit);
-	rec->h_color = create_vec((double)(cylinder->clr.r) / 255, (double)(cylinder->clr.g) / 255, (double)(cylinder->clr.b) / 255);
+	if (param.b <= eps || (param.c > eps && (param.c < param.b)))
+		param.b = param.c;
+	return (rec->t = param.b, true);
 	
-	if (pow(distance(cylinder->cord, rec->pHit), 2) > (pow(cylinder->height * 0.5, 2) + pow(cylinder->diameter/2, 2)))
-		printf("out of cylinder %.1f %.1f  %.1f\n", rec->pHit.v_x, rec->pHit.v_y, rec->pHit.v_z);
+}
+void calculate_disk_plan(t_cylender *cylinder, t_objects *obj, bool	is_top)
+{
+	t_plane	*plan;
+	plan = obj->object;
+
+	plan->normalized = cylinder->normalized;
+	if (is_top)
+		plan->cord = vec_addition(cylinder->cord, scalar_mult(cylinder->normalized, cylinder->height * 0.5));
 	else
-		printf("in of cylinder %.1f %.1f  %.1f\n", rec->pHit.v_x, rec->pHit.v_y, rec->pHit.v_z);
-	
+		plan->cord = vec_sub(cylinder->cord, scalar_mult(cylinder->normalized, cylinder->height * 0.5));
+}
+
+static bool	cylinder_hit(t_ray *ray, t_cylender *cylinder, t_hit_record *rec)
+{
+	// t_cylender	*cylinder;
+	t_cord		p;
+	t_vec		v;
+
+	// cylinder = obj->object;
+	if (!solve_quad(ray, cylinder, rec))
+		return false;
+	rec->pHit = at(rec->t, *ray);
+	v = vec_sub(rec->pHit, cylinder->cord);
+	rec->nHit = normalized(cross_product(cross_product(v, cylinder->normalized), cylinder->normalized));
+	if (dot_product(rec->nHit, ray->direction))
+		rec->nHit = vec_nega(rec->nHit);
+	if (pow(distance(cylinder->cord, rec->pHit), 2) > (pow(cylinder->height * 0.5, 2) + pow(cylinder->diameter * 0.5, 2)))
+		return false;
+	rec->h_color = create_vec((double)(cylinder->clr.r) / 255, (double)(cylinder->clr.g) / 255, (double)(cylinder->clr.b) / 255);
 	return true;
-	// p.a = dot_product(ray->direction, ray->direction) - dot_product(ray->direction, cylinder->normalized) * dot_product(ray->direction, cylinder->normalized);
-	// p.b = (dot_product(ray->direction, oc) - dot_product(ray->direction, cylinder->normalized) * dot_product(oc, cylinder->normalized));
-	// p.c = dot_product(oc, oc) - dot_product(oc, cylinder->normalized) * dot_product(oc, cylinder->normalized) - (cylinder->diameter / 2) * (cylinder->diameter / 2);
-	// discriminant = p.b * p.b - (p.a * p.c);
-	// if (discriminant < eps)
-	// 	return (false);
-	// if (discriminant > eps)
+}
+
+bool	f_cylinder_render(t_ray *ray, t_objects *obj, t_hit_record *rec)
+{
+	t_cylender	*cylinder;
+	t_objects		*plan;
+	rec->t = M_D;
+	plan = malloc(sizeof(t_objects));
+	plan->next = NULL;
+	plan->object = malloc(sizeof(t_plane));
+	ft_memset(plan->object, 0, sizeof(t_plane));
+	t_hit_record	tmp_rec;
+	cylinder = obj->object;
+	calculate_disk_plan(cylinder, plan,true);
+	((t_plane *)(plan->object))->clr = cylinder->clr;
+	// printf("plan type %f %f %f\n", ((t_plane *)(plan->object))->cord.v_x, ((t_plane *)(plan->object))->cord.v_y, ((t_plane *)(plan->object))->cord.v_z);
+	// printf("obj->type %d\n", plan.type);
+		
+	calculate_disk_plan(cylinder, plan, false);
+	// printf("plan2 type %f %f %f\n", ((t_plane *)(plan->object))->cord.v_x, ((t_plane *)(plan->object))->cord.v_y, ((t_plane *)(plan->object))->cord.v_z);
+
+	// if (plan_hit(ray, plan, &tmp_rec)
+	// 	&& distance(tmp_rec.pHit, ((t_plane *)(plan->object))->cord) <= ((cylinder->diameter * 0.5) - 0.002)
+	// 	&& rec->t > tmp_rec.t)
 	// {
-	// 	tmp = (-p.b - sqrt(discriminant)) / (p.a);
-	// 	if (tmp <= 0.0 || tmp >= M_D)
-	// 	{
-	// 		tmp = (-p.b + sqrt(discriminant)) / (p.a);
-	// 		if (tmp <= 0.0 || tmp >= M_D)
-	// 			return (false);		
-	// 	}
-	// }
-	// else
-	// {
-	// 	tmp = -p.b / p.a;
-	// 	if (tmp <= 0.0 || tmp >= M_D)
-	// 		return (false);
-	// }
-	// rec->t = tmp;
-	// rec->pHit = at(rec->t, *ray);
-	// rec->nHit = vec_sub(rec->pHit, cylinder->cord);
-	// rec->h_color = create_vec((double)(cylinder->clr.r) / 255, (double)(cylinder->clr.g) / 255, (double)(cylinder->clr.b) / 255);
-	// return (true);
+	// 	*rec = tmp_rec;
+	// 	puts("here");
+	// }	
+	if (cylinder_hit(ray, cylinder, &tmp_rec)
+		&& pow(distance(cylinder->cord, tmp_rec.pHit), 2) <= pow(cylinder->height * 0.5, 2) + pow(cylinder->diameter * 0.5, 2))
+		*rec = tmp_rec;
+	return (rec->t < M_D && rec->t > eps);
 }
 
 inter_func	intersect(int type)
@@ -180,6 +198,6 @@ inter_func	intersect(int type)
 	inter_func 		obj_inter[3];
 	obj_inter[SPHERE] = sphere_hit;
 	obj_inter[PLANE] = plan_hit;
-	obj_inter[CYLENDER] = cylinder_hit;
+	obj_inter[CYLENDER] = f_cylinder_render;
 	return (*(obj_inter + type));
 }
