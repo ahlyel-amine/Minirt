48dbad6b4687c6fb47204d6281f518e91b57e666
bleuuone <bleuuone@gmail.com>
2023-12-11 04:13:09 +0100

    CHECKER

--- src/minirt/camera.c
+++ src/minirt/camera.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 06:33:28 by aelbrahm          #+#    #+#             */
-/*   Updated: 2023/12/02 13:21:37 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/12/11 04:12:50 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,32 +23,6 @@ t_camera *create_cam(t_vec *origin, t_vec *direction, double fov)
 		return (NULL);
 	return cam;
 }
-// t_vector	bump_normal(t_shape s, t_vector n, t_vector eye, t_point p)
-// {
-// 	int			c[2];
-// 	int			x[2];
-// 	int			y[2];
-// 	double		b_scale[2];
-// 	t_tools		b;
-
-// 	b.t = s.bump_img;
-// 	b.uv = s.mapping.uv_map(mul_mat_point(s.ivers_tran, p));
-// 	x[0] = b.uv.u * (b.t->width - 1);
-// 	y[0] = (1 - b.uv.v) * (b.t->height - 1);
-// 	y[1] = (y[0] + 1) % b.t->height;
-// 	x[1] = (x[0] + 1) % b.t->width;
-// 	c[0] = ((int *)b.t->pixels)[x[0] + y[0] * b.t->width] & 255;
-// 	c[1] = ((int *)b.t->pixels)[x[0] + y[1] * b.t->width] & 255;
-// 	b_scale[0] = ((double)c[1] - c[0]) / 9;
-// 	c[1] = ((int *)b.t->pixels)[x[1] + y[0] * b.t->width] & 255;
-// 	b_scale[1] = ((double)c[1] - c[0]) / 9;
-// 	b.pu = cross_product(n, eye);
-// 	b.pv = cross_product(n, b.pu);
-// 	b.n_ = n;
-// 	b.n_ = add_to_vector(b.n_, scaler_vect(cross_product(b.pu, n), b_scale[1]));
-// 	b.n_ = add_to_vector(b.n_, scaler_vect(cross_product(b.pv, n), b_scale[0]));
-// 	return (normalize(b.n_));
-// }
 t_vec	cam_to_world(double matrix[4][4], t_vec *dir)
 {
 	t_vec	v;	
