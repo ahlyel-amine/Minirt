bb7a6467fa9f76c6d2da4bc3d728f48b85695624
bleuuone <bleuuone@gmail.com>
2023-12-12 12:16:15 +0100

    hooks_add

--- src/tracer/rt_hooks.c
+++ src/tracer/rt_hooks.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:38:10 by aelbrahm          #+#    #+#             */
-/*   Updated: 2023/12/12 10:40:22 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/12/12 12:15:16 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "libft.h"
 #include "library.h"
 #include "tools.h"
+#include "vector.h"
 #include <mlx.h>
 
 void	close_key(t_mrt *scean)
@@ -25,40 +26,120 @@ void	close_key(t_mrt *scean)
 }
 void	cam_x(int key, t_data *data)
 {
-	if (key == 123)
-	{	
-		if (data->camera.normalized.v_x < 1.0)
-			data->camera.normalized.v_x += 0.2;
-	}
-	else if (key == 124)
-	{
-		if (data->camera.normalized.v_x > -1.0)
-		data->camera.normalized.v_x -= 0.2;
-	}
-	if (data->camera.normalized.v_x < 1 && data->camera.normalized.v_x > -1)
-	{
-		lookat(&(data->camera));
-		draw(*data, data->m_rt);
-	}
+	if (key == 124)
+		data->camera.normalized.v_x += 0.5;
+	else if (key == 123)
+		data->camera.normalized.v_x -= 0.5;
+	normalize(&data->camera.normalized);
+	lookat(&(data->camera));
+	draw(*data, data->m_rt);
 }
 
 void	cam_y(int key, t_data *data)
 {
 	if (key == 126)
-	{	
-		if (data->camera.normalized.v_y < 1.0)
-			data->camera.normalized.v_y += 0.2;
-	}
+			data->camera.normalized.v_y += 0.4;
 	else if (key == 125)
+		data->camera.normalized.v_y -= 0.4;
+	normalize(&data->camera.normalized);
+	lookat(&(data->camera));
+	draw(*data, data->m_rt);
+}
+void	zoom(int key, t_data *data)
+{
+	if (key == 69)
+			data->camera.cord.v_z += 5;
+	else if (key == 78)
+			data->camera.cord.v_z -= 5;
+	lookat(&(data->camera));
+	draw(*data, data->m_rt);
+}
+
+void	cylinders_rotate(t_data *data, t_cylender *c, int key)
+{
+	if (key == 38)
+		c->normalized.v_x += 0.5;
+	else if (key == 37)
+		c->normalized.v_x -= 0.5;
+	else if (key == 34)
+		c->normalized.v_z += 0.5;
+	else if (key == 40)
+		c->normalized.v_z -= 0.5;
+	normalize(&c->normalized);
+}
+void	plane_rotate(t_data *data, t_plane *p, int key)
+{
+	if (key == 38)
+		p->normalized.v_x += 0.1;
+	else if (key == 37)
+		p->normalized.v_x -= 0.1;
+	else if (key == 34)
+		p->normalized.v_y += 0.1;
+	else if (key == 40)
+		p->normalized.v_y -= 0.1;
+	normalize(&p->normalized);
+}
+void	rotate(int key, t_data *data)
+{
+	t_plane		*p;
+	t_cylender	*c;
+	t_objects	*obj;
+
+	obj = data->objects;
+	while (obj)
 	{
-		if (data->camera.normalized.v_y > -1.0)
-		data->camera.normalized.v_y -= 0.2;
+		if (data->shape == CYLENDER && obj->type == CYLENDER)
+			cylinders_rotate(data, obj->object, key);
+		else if (data->shape == PLANE && obj->type == PLANE)
+			plane_rotate(data, obj->object, key);
+		obj = obj->next;	
 	}
-	if (data->camera.normalized.v_y < 1 && data->camera.normalized.v_y > -1)
+	lookat(&(data->camera));
+	draw(*data, data->m_rt);
+}
+void	sphere_scale(t_data *data, int key)
+{
+	t_objects *obj;
+
+	obj = data->objects;
+	while (obj)
 	{
-		lookat(&(data->camera));
-		draw(*data, data->m_rt);
+		if (obj->type == SPHERE)
+		{
+			if (key == 27)
+				((t_sphere *)obj->object)->diameter -= 0.1;
+			else if (key == 24)
+				((t_sphere *)obj->object)->diameter += 0.1;
+		}
+		obj = obj->next;
 	}
+}
+
+void	cy_scale(t_data *data, int key)
+{
+	t_objects *obj;
+
+	obj = data->objects;
+	while (obj)
+	{
+		if (obj->type == CYLENDER)
+		{
+			if (key == 27)
+				((t_cylender *)obj->object)->height -= 0.1;
+			else if (key == 24)
+				((t_cylender *)obj->object)->height += 0.1;
+		}
+		obj = obj->next;
+	}
+}
+
+void	scale(int key, t_data *data)
+{
+	if (data->shape == SPHERE)
+		sphere_scale(data, key);
+	else if (data->shape == CYLENDER)
+		cy_scale(data, key);
+	draw(*data, data->m_rt);
 }
 int	key_hook(int keycode, t_data *data)
 {
@@ -68,6 +149,18 @@ int	key_hook(int keycode, t_data *data)
 		cam_x(keycode, data);
 	if (keycode == 125 || keycode == 126)
 		cam_y(keycode, data);
+	if (keycode == 78 || keycode == 69)
+		zoom(keycode, data);
+	if (keycode == 8)
+		data->shape = CYLENDER;
+	if (keycode == 35)
+		data->shape = PLANE;
+	if (keycode == 1)
+		data->shape = SPHERE;
+	if (data->shape && (keycode == 38 || keycode == 37 || keycode == 34 || keycode == 40))
+		rotate(keycode, data);
+	if ((keycode == 24 || keycode == 27))
+		scale(keycode, data);
 	printf("%d\n", keycode);
 	return (0);
 }
