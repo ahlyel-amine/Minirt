97aae59eb3b77b9c4f97b6cdc0f0beeb7c3aa4aa
bleuuone <bleuuone@gmail.com>
2023-12-17 10:37:44 +0100

    SPHERE_NORM

--- bonus/src_bonus/tracer_bonus/intersections_bonus/sphere_intersections_bonus.c
+++ bonus/src_bonus/tracer_bonus/intersections_bonus/sphere_intersections_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 16:07:54 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/12/14 18:33:43 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/12/17 10:34:56 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,55 @@
 #include "vector_bonus.h"
 #include "library_bonus.h"
 
+double	sphere_quad(t_cord *p, t_objects *shape, t_ray *ray)
+{
+	t_vec		oc;
+	t_sphere	*sphere;
+	double		discriminant;
+
+	sphere = shape->object;
+	oc = vec_sub(ray->origin, sphere->cord);
+	p->a = dot_product(ray->direction, ray->direction);
+	p->b = dot_product(oc, ray->direction);
+	p->c = dot_product(oc, oc) - pow(sphere->diameter * 0.5, 2);
+	return (discriminant = p->b * p->b - (p->a * p->c));
+}
+
+void	update_record(t_hit_record *rec, t_sphere *s, t_ray *r, double distance)
+{
+	rec->t = distance;
+	rec->p_hit = at(distance, *r);
+	rec->n_hit = vec_sub(rec->p_hit, s->cord);
+	normalized(rec->n_hit);
+	rec->h_color = create_vec((double)(s->clr.r) / 255, \
+	(double)(s->clr.g) / 255, (double)(s->clr.b) / 255);
+}
+
 bool	sphere_hit(t_ray *ray, t_objects *obj, t_hit_record *rec)
 {
-	t_vec       oc;
-	t_cord      p;
-	double      discriminant;
-	double      tmp;
-	t_sphere    *sphere;
+	double	discriminant;
+	double	tmp;
+	t_cord	p;
 
-    sphere = obj->object;
-	oc = vec_sub(ray->origin, sphere->cord);
-	p.a = dot_product(ray->direction, ray->direction);
-	p.b = dot_product(oc, ray->direction);
-	p.c = dot_product(oc, oc) - pow(sphere->diameter/2, 2);
-	discriminant = p.b * p.b - (p.a * p.c);
-	if (discriminant < eps)
+	discriminant = sphere_quad(&p, obj, ray);
+	if (discriminant < EPS)
 		return (false);
-	if (discriminant > eps)
+	if (discriminant > EPS)
 	{
 		tmp = (-p.b - sqrt(discriminant)) / (p.a);
-		if (tmp <= eps || tmp >= M_D)
+		if (tmp <= EPS || tmp >= M_D)
 		{
 			tmp = (-p.b + sqrt(discriminant)) / (p.a);
-			if (tmp <= eps || tmp >= M_D)
-				return (false);		
+			if (tmp <= EPS || tmp >= M_D)
+				return (false);
 		}
 	}
 	else
 	{
 		tmp = -p.b / p.a;
-		if (tmp <= eps || tmp >= M_D)
+		if (tmp <= EPS || tmp >= M_D)
 			return (false);
 	}
-	rec->t = tmp;
-	rec->p_hit = at(rec->t, *ray);
-	t_vec norm = vec_sub(rec->p_hit, sphere->cord);
-	rec->n_hit = normalize(&norm);
-	rec->h_color = create_vec((double)(sphere->clr.r) / 255, (double)(sphere->clr.g) / 255, (double)(sphere->clr.b) / 255);
+	update_record(rec, (t_sphere *)(obj->object), ray, tmp);
 	return (true);
 }
