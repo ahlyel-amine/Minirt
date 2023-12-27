aae3d56be039d120d8c8dca229b0a5c6730efc56
bleuuone <bleuuone@gmail.com>
2023-12-27 21:40:05 +0100

    ADD_CAPS_MAP

--- bonus/src_bonus/tracer_bonus/tools_bonus.c
+++ bonus/src_bonus/tracer_bonus/tools_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:11:38 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/12/27 15:28:52 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/12/27 21:38:33 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,13 +62,25 @@ void	get_uv_cylinder(t_cylender *cy, t_hit_record *rec, double *u, double *v)
 {
 	t_vec	e1;
 	t_vec	e2;
+	double	theta;
+	double	h_z;
 
-	e1 = normalized(cross_product(cy->normalized, (t_vec){1, 0, 0}));
-	if (e1.v_x == 0 && e1.v_y == 0 && e1.v_z == 0)
-		e1 = normalized(cross_product(cy->normalized, (t_vec){0, 0, 1}));
-	e2 = normalized(cross_product(cy->normalized, e1));
-	*u = dot_product(e1, rec->p_hit);
-	*v = dot_product(e2, rec->p_hit);
+	if (distance(rec->p_hit, cy->cord) <= (cy->diameter * 0.5))
+	{
+		e1 = normalized(cross_product(cy->normalized, (t_vec){1, 0, 0}));
+		if (e1.v_x == 0 && e1.v_y == 0 && e1.v_z == 0)
+			e1 = normalized(cross_product(cy->normalized, (t_vec){0, 0, 1}));
+		e2 = normalized(cross_product(cy->normalized, e1));
+		*u = dot_product(e1, rec->p_hit);
+		*v = dot_product(e2, rec->p_hit);
+	}
+	else
+	{
+		theta = atan2(rec->p_hit.v_z, rec->p_hit.v_x);
+		h_z = rec->p_hit.v_y;
+		*u = theta / (2 * M_PI);
+		*v = h_z / cy->height;
+	}
 }
 
 void	skip(int key, t_data *data)
